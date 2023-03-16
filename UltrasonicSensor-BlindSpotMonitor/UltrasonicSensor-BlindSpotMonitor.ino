0f71df77550cd462ce59486e950875d7223e2972
Tyler Mattson <02tyler.mattson@gulllakecs.org>
2023-03-16 14:07:45 -0400

    Initial Blind Spot Monitor Commit

---
+++ UltrasonicSensor-BlindSpotMonitor/UltrasonicSensor-BlindSpotMonitor.ino
@@ -0,0 +1,19 @@
+//Programmer: Tyler Mattson
+//Date: 3.16.2023
+//Program: Blind Spot Monitor
+
+/*
+ * This program will determine distances approaching vehicles that are in your blind spot and it will also turn on a warning light.
+ */
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
